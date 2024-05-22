8eb82706ae2e5bb242894fb1ba9d56b3b324cfea
katya1331 <152582001+katya1331@users.noreply.github.com>
2024-05-22 11:20:59 +0500

    Add files via upload

---
+++ pr16z4/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace pr16z4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(396, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вывести страны";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(396, 277);
+            this.listBox1.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 23);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1500000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(396, 26);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 404);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ pr16z4/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace pr16z4
+{
+    public partial class Form1 : Form
+    {
+        class Country
+        {
+            public string Name { get; set; }
+            public int Population  { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (File.Exists("1.txt"))
+            {
+                int chislenost = (int)numericUpDown1.Value;
+                if (chislenost != 0)
+                {
+                    List<Country> countries = File.ReadAllLines("1.txt")
+                    .Select(line => line.Split(' '))
+                    .Select(parts => new Country { Name = parts[0], Population = Convert.ToInt32(parts[1]) }).ToList();
+                foreach (var cc in countries)
+                {
+                    listBox1.Items.Add(cc.Name + " " + cc.Population);
+                }
+                listBox1.Items.Add("");
+                listBox1.Items.Add($"Упорядоченный список стран, у которых численность больше {chislenost}:");
+                
+                    var SortCountry = countries.Where(x => x.Population > chislenost)
+                        .OrderBy(x => x.Name.Length)
+                        .ThenBy(x => x.Name)
+                        .ToList();
+                    foreach (var cc in SortCountry)
+                    {
+                        listBox1.Items.Add(cc.Name + " " + cc.Population);
+                    }
+                }
+                else MessageBox.Show("ВВедите число,отичное от нуля");
+            }
+            else MessageBox.Show("File don't exist");
+        }
+
+    }
+}
